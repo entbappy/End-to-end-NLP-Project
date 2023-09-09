6f3470c60955c3cdadd2d43a9c3218d1d5eebeba
entbappy <entbappy73@gmail.com>
2023-09-09 11:44:10 +0600

    data ingestion added

---
+++ main.py
@@ -0,0 +1,13 @@
+from textClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textClassification.logging import logger
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/textClassification/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,16 @@
+from textClassification.config.configuration import ConfigurationManager
+from textClassification.conponents.data_ingestion import DataIngestion
+from textClassification.logging import logger
+
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
